948eac36651bb2668611bdedc16fd29ba6f9c240
Adarsh Patnaik <adarshpatnaik13031999@gmail.com>
2019-06-24 07:10:08 +0530

    added ode solver

--- target_tracking/split_control_law.m
+++ target_tracking/split_control_law.m
@@ -11,10 +11,13 @@ global w = 3;
 global C1 = 2; global l1 = 0.5; global C2 = 1; global l2 = 1;
 global b = 1.39;
 num_robots = 100;
-betaa = sqrt(5)/10;
+global betaa = sqrt(5)/10;
+
 max_iter = 500;
+tsamp = 0.01;
+tspan = [0, tsamp];
 %initialize lambda
-lambda = 0;
+global lambda = 0;
 
 %% initialize swarm agents
 u_ji = zeros(num_robots, 1);
@@ -25,13 +28,18 @@ St = zeros(num_robots, 3);
 Rji_mod = zeros(num_robots, num_robots);
 Rji_vec = zeros(num_robots, num_robots, 2);
 %initialize distances matrix
-Rji_mod = get_dist_mat(S0);
-Rji_vec = get_relative_mat(S0);
-u_ji = get_uji(Rji_vec, Rji_mod, S0)
 
 for j = 1:max_iter
 	j
+	Rji_mod = get_dist_mat(S0);
+	Rji_vec = get_relative_mat(S0);
+	u_ji = get_uji(Rji_vec, Rji_mod, S0);
+
 	for i = 1:num_robots
-		ui = sum(u_ji(:, i), 2) - u_ji(i, i)
+		ui = sum(u_ji(:, i), 2) - u_ji(i, i);
+		[t, x1] = ode45(@split_law, tspan, [S0(i), u(i)]);
+		S0(i) = x1(end, 1:3);
+	end
+end
 		
 		
